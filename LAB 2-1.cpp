96a52ddfde2b2c287122a2744b768772caaf0895
ahad raza <94754804+CODEWITHAHAD@users.noreply.github.com>
2021-11-20 21:46:14 +0500

    Delete LAB 2-1.cpp

--- LAB 2-1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"NAME:ABDUL AHAD RAZA"<<endl;
-	cout<<"DEPARTMENT:BSCS(MORNING)"<<endl;
-	cout<<"NAME OF YOUR CLASS:BSCS(36)"<<endl;
-	return(0);
-}
